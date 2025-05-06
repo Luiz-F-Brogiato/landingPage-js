4082af4686a5ce5bd4bdff74b37fc13d597b0fc8
Luiz-F-Brogiato <brogiatoluizfernando@gmail.com>
2025-05-06 14:19:41 -0300

    Lógica básica

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 20;
+
+let cor = "Amarelo";
+let cor2 = "Verde";
+
+if(cor === "Amarelo" && cor2 === "Verde") {
+    console.log("A cor é amarelo e verde")
+} else {
+    console.log("Não é amarelo nem verde")
+}
