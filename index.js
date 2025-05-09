b1fb21266017551656af78dd4201d4f00748486e
Luiz-F-Brogiato <brogiatoluizfernando@gmail.com>
2025-05-09 17:49:48 -0300

    Código inicial

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ let n = 5;
 for(let i = 0; i <= 10; i++) {
     console.log(`${i} X ${n} = ${i*n}`);
 }
+
+function calc(x1, x2, operator){
+    return eval(`${x1} ${operator} ${x2}`)
+}
+
+let resultado = calc(1, 2, "^");
+
+console.log(resultado)
+
+let calqn = (x1, x2, operator) => {
+    return eval(`${x1} ${operator} ${x2}`)
+}
+
